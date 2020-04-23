0eb6bffce0954ce6963a8fb619a2fa2d7eae11da
faizshiraji <faizshiraji@gmail.com>
2020-04-23 21:42:14 +0600

    First Commit.

---
+++ customerdal/src/test/java/com/doller/customerdal/CustomerdalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doller.customerdal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerdalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
