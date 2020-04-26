3e27513a6aeca565d0d273b797a5774d18dc6b6b
faizshiraji <faizshiraji@gmail.com>
2020-04-26 19:39:05 +0600

    Final with Full API !!!

--- locationweb/src/main/java/com/doller/location/controllers/LocationRESTController.java
+++ locationweb/src/main/java/com/doller/location/controllers/LocationRESTController.java
@@ -3,8 +3,11 @@ package com.doller.location.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +31,23 @@ public class LocationRESTController {
 	@PostMapping
 	public Location createLocation(@RequestBody Location location) {
 		return locationRepository.save(location);
+		
 	}
+	
+	@PutMapping
+	public Location updateLocation(@RequestBody Location location) {
+		return locationRepository.save(location);
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteLocation(@PathVariable("id") int id) {
+		locationRepository.deleteById(id);
+	}
+	
+	@GetMapping("/{id}")
+	public Location getLocation(@PathVariable("id") int id) {
+		return locationRepository.findById(id).get();
+	}
+	
 }
