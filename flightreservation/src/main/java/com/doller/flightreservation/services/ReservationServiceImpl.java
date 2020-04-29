6dd9c2b049d859be8c280e34613dd8df0c3545b6
faizshiraji <faizshiraji@gmail.com>
2020-04-29 15:34:34 +0600

    With PDF and Mail Module Updated !!!

--- flightreservation/src/main/java/com/doller/flightreservation/services/ReservationServiceImpl.java
+++ flightreservation/src/main/java/com/doller/flightreservation/services/ReservationServiceImpl.java
@@ -10,44 +10,55 @@ import com.doller.flightreservation.entities.Reservation;
 import com.doller.flightreservation.repos.FlightRepository;
 import com.doller.flightreservation.repos.PassengerRepository;
 import com.doller.flightreservation.repos.ReservationRepository;
+import com.doller.flightreservation.util.EmailUtil;
+import com.doller.flightreservation.util.PDFGenerator;
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
 
-	
 	@Autowired
 	FlightRepository flightRepository;
-	
+
 	@Autowired
 	PassengerRepository passengerRepository;
-	
+
 	@Autowired
 	ReservationRepository reservationRepository;
-	
+
+	@Autowired
+	PDFGenerator pdfGenerator;
+
+	@Autowired
+	EmailUtil emailUtil;
+
 	@Override
 	public Reservation bookFlight(ReservationRequest request) {
 
-		//Make Payment
-		
+		// Make Payment
+
 		Long flightId = request.getFlightId();
 		Flight flight = flightRepository.findById(flightId).get();
-		
+
 		Passenger passenger = new Passenger();
 		passenger.setFirstName(request.getPassengerFirstName());
 		passenger.setLastName(request.getPassengerLastName());
 		passenger.setPhone(request.getPassengerPhone());
 		passenger.setEmail(request.getPassengerEmail());
-		
+
 		Passenger savedPassenger = passengerRepository.save(passenger);
-		
+
 		Reservation reservation = new Reservation();
 		reservation.setFlight(flight);
 		reservation.setPassenger(savedPassenger);
 		reservation.setCheckedIn(false);
-		
+
 		Reservation savedReservation = reservationRepository.save(reservation);
 		
-		
+		String filePath = "C:/Users/dolow/OneDrive/" + "doller_reservation_pdfs_"
+				+ savedReservation.getId() + ".pdf";
+		pdfGenerator.generateItinerary(savedReservation, filePath);
+		emailUtil.sendItinerary(passenger.getEmail(), filePath);
+		System.out.println(filePath);
 		return savedReservation;
 	}
 
